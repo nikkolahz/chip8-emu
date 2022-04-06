dd5c7512406b9d47fdfffa0307727a336e8da8c1
nikkolahz <nikko.v.salcedo@gmail.com>
2022-04-06 13:33:16 +0800

    Added a chip8 game file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::{fs::File, io::Read};
+
 fn main() {
-    println!("Hello, world!");
+let mut file = File::open("data/INVADERS").unwrap();
+let mut data = Vec::<u8>::new();
+let _ =file.read_to_end(&mut data);
+println!("{:?}", data);
 }
